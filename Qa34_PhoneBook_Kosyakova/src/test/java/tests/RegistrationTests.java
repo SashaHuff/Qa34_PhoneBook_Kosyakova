a83e08d0dca8173487a57a02f49ce504809daa56
SashaHuff <107310874+SashaHuff@users.noreply.github.com>
2022-07-05 09:11:12 +0300

    gq

--- Qa34_PhoneBook_Kosyakova/src/test/java/tests/RegistrationTests.java
+++ Qa34_PhoneBook_Kosyakova/src/test/java/tests/RegistrationTests.java
@@ -1,14 +1,22 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class RegistrationTests extends TestBase
 {
+    @BeforeMethod
+    public void preCondition() {
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logOut();
+        }
+
+    }
     @Test
     public void registrationSuccess()
     {
         app.getHelperUser().openLoginRegistrationForm();
-        app.getHelperUser().fillLoginRegistrationForm("Test1234@gmail.com","Ttest1234$");
+        app.getHelperUser().fillLoginRegistrationForm("Test1278@gmail.com","Ttest1278$");
         app.getHelperUser().submitRegistration();
     }
 }
